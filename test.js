0b3e6dd8c9423929bec9bd8f015b25c86644b8a1
Titus Wormer <tituswormer@gmail.com>
2016-07-12 11:25:05 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,34 @@
+/**
+ * @author Titus Wormer
+ * @copyright 2016 Titus Wormer
+ * @license MIT
+ * @module is-whitespace-character
+ * @fileoverview Test suite for `is-whitespace-character`.
+ */
+
+'use strict';
+
+/* eslint-env node */
+
+/* Dependencies. */
+var test = require('tape');
+var whitespace = require('./');
+
+/* Tests. */
+test('whitespace(character)', function (t) {
+  t.ok(whitespace(' '));
+  t.ok(whitespace('\n'));
+  t.ok(whitespace('\r'));
+  t.ok(whitespace('\ufeff'));
+  t.ok(whitespace('\u00a0'));
+  t.ok(whitespace('\t'));
+  t.ok(whitespace('\v'));
+  t.ok(whitespace(' '.charCodeAt(0)));
+  t.ok(whitespace('\n'.charCodeAt(0)));
+  t.ok(whitespace('\u1680'.charCodeAt(0)));
+  t.notOk(whitespace('a'));
+  t.notOk(whitespace(' '.charCodeAt(0) - 1));
+  t.notOk(whitespace('💩'));
+
+  t.end();
+});
