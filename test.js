bc7cee9ef7e3febf4843e691988473ad355478d4
Titus Wormer <tituswormer@gmail.com>
2018-04-21 11:44:52 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var whitespace = require('.');
+var test = require('tape')
+var whitespace = require('.')
 
-test('whitespace(character)', function (t) {
-  t.ok(whitespace(' '));
-  t.ok(whitespace('\n'));
-  t.ok(whitespace('\r'));
-  t.ok(whitespace('\uFEFF'));
-  t.ok(whitespace('\u00A0'));
-  t.ok(whitespace('\t'));
-  t.ok(whitespace('\v'));
-  t.ok(whitespace(' '.charCodeAt(0)));
-  t.ok(whitespace('\n'.charCodeAt(0)));
-  t.ok(whitespace('\u1680'.charCodeAt(0)));
-  t.notOk(whitespace('a'));
-  t.notOk(whitespace(' '.charCodeAt(0) - 1));
-  t.notOk(whitespace('💩'));
+test('whitespace(character)', function(t) {
+  t.ok(whitespace(' '))
+  t.ok(whitespace('\n'))
+  t.ok(whitespace('\r'))
+  t.ok(whitespace('\uFEFF'))
+  t.ok(whitespace('\u00A0'))
+  t.ok(whitespace('\t'))
+  t.ok(whitespace('\v'))
+  t.ok(whitespace(' '.charCodeAt(0)))
+  t.ok(whitespace('\n'.charCodeAt(0)))
+  t.ok(whitespace('\u1680'.charCodeAt(0)))
+  t.notOk(whitespace('a'))
+  t.notOk(whitespace(' '.charCodeAt(0) - 1))
+  t.notOk(whitespace('💩'))
 
-  t.end();
-});
+  t.end()
+})
