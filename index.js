d1e9ca96ae180fa12507c2f6b6f865dac9580a5b
Titus Wormer <tituswormer@gmail.com>
2020-10-28 10:20:54 +0100

    Refactor code to improve bundle size

--- index.js
+++ index.js
@@ -2,13 +2,12 @@
 
 module.exports = whitespace
 
-var fromCode = String.fromCharCode
-var re = /\s/
-
 // Check if the given character code, or the character code at the first
 // character, is a whitespace character.
 function whitespace(character) {
-  return re.test(
-    typeof character === 'number' ? fromCode(character) : character.charAt(0)
+  return /\s/.test(
+    typeof character === 'number'
+      ? String.fromCharCode(character)
+      : character.charAt(0)
   )
 }
