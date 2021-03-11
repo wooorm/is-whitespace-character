f02dcab9f46bf2db91893eb8fda5714010ffcdc0
Titus Wormer <tituswormer@gmail.com>
2021-03-11 09:22:50 +0100

    Add types

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-// Check if the given character code, or the character code at the first
-// character, is a whitespace character.
+/**
+ * Check if the given character code, or the character code at the first
+ * character, is a whitespace character.
+ *
+ * @param {string|number} character
+ * @returns {boolean} Whether `character` is a whitespace character
+ */
 export function isWhitespaceCharacter(character) {
   return /\s/.test(
     typeof character === 'number'
