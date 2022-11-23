fe17d75852cff0753d2a457c163c5ac6db7926bb
Titus Wormer <tituswormer@gmail.com>
2022-11-23 12:14:34 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,20 +1,19 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {isWhitespaceCharacter} from './index.js'
 
-test('isWhitespaceCharacter(character)', function (t) {
-  t.ok(isWhitespaceCharacter(' '))
-  t.ok(isWhitespaceCharacter('\n'))
-  t.ok(isWhitespaceCharacter('\r'))
-  t.ok(isWhitespaceCharacter('\uFEFF'))
-  t.ok(isWhitespaceCharacter('\u00A0'))
-  t.ok(isWhitespaceCharacter('\t'))
-  t.ok(isWhitespaceCharacter('\v'))
-  t.ok(isWhitespaceCharacter(0x20))
-  t.ok(isWhitespaceCharacter(0xa))
-  t.ok(isWhitespaceCharacter(0x16_80))
-  t.notOk(isWhitespaceCharacter('a'))
-  t.notOk(isWhitespaceCharacter(0x19))
-  t.notOk(isWhitespaceCharacter('💩'))
-
-  t.end()
+test('isWhitespaceCharacter(character)', function () {
+  assert.ok(isWhitespaceCharacter(' '))
+  assert.ok(isWhitespaceCharacter('\n'))
+  assert.ok(isWhitespaceCharacter('\r'))
+  assert.ok(isWhitespaceCharacter('\uFEFF'))
+  assert.ok(isWhitespaceCharacter('\u00A0'))
+  assert.ok(isWhitespaceCharacter('\t'))
+  assert.ok(isWhitespaceCharacter('\v'))
+  assert.ok(isWhitespaceCharacter(0x20))
+  assert.ok(isWhitespaceCharacter(0xa))
+  assert.ok(isWhitespaceCharacter(0x16_80))
+  assert.ok(!isWhitespaceCharacter('a'))
+  assert.ok(!isWhitespaceCharacter(0x19))
+  assert.ok(!isWhitespaceCharacter('💩'))
 })
